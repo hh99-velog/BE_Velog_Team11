6ceaf8b68a2da59b867ef6fc2dce3520c3674a12
Jonguk Kim <kju190920@gmail.com>
2021-12-15 14:34:02 +0900

    생성시간/수정시간 자동 설정

--- src/main/java/com/example/be_velog_team11/BeVelogTeam11Application.java
+++ src/main/java/com/example/be_velog_team11/BeVelogTeam11Application.java
@@ -2,7 +2,9 @@ package com.example.be_velog_team11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BeVelogTeam11Application {
 
