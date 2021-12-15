91c0ebcc04672c35316a7dc2e1dd80412a80a1e4
Jonguk Kim <kju190920@gmail.com>
2021-12-15 10:00:23 +0900

    first commit

---
+++ src/main/java/com/example/be_velog_team11/BeVelogTeam11Application.java
@@ -0,0 +1,13 @@
+package com.example.be_velog_team11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeVelogTeam11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeVelogTeam11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/be_velog_team11/BeVelogTeam11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_velog_team11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeVelogTeam11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
