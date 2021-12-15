fb4d74e4c2928df73894db65a3d5244bd7e05417
woojin126 <woojin126@naver.com>
2021-12-15 17:30:44 +0900

    Make Annotation User exist discrimination

---
+++ src/main/java/com/example/be_velog_team11/config/LoginUser.java
@@ -0,0 +1,14 @@
+package com.example.be_velog_team11.config;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : user")
+public @interface LoginUser {
+}
