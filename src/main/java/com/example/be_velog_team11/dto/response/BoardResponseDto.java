5eac2c56aaef221e57a5d5eaca42661c7e7a2f9d
woojin126 <woojin126@naver.com>
2021-12-16 13:20:43 +0900

    BoardResponseDto

--- src/main/java/com/example/be_velog_team11/dto/response/BoardResponseDto.java
+++ src/main/java/com/example/be_velog_team11/dto/response/BoardResponseDto.java
@@ -1,7 +1,6 @@
 package com.example.be_velog_team11.dto.response;
 
 
-import com.example.be_velog_team11.model.Likes;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
