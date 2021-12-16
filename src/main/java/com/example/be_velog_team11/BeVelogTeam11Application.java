b32391818b729579bc086f70f1f87335cd7bc9d8
woojin126 <woojin126@naver.com>
2021-12-16 12:56:00 +0900

    add Time Convert korea

--- src/main/java/com/example/be_velog_team11/BeVelogTeam11Application.java
+++ src/main/java/com/example/be_velog_team11/BeVelogTeam11Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class BeVelogTeam11Application {
@@ -17,5 +20,10 @@ public class BeVelogTeam11Application {
                 .properties(APPLICATION_LOCATIONS)
                 .run(args);
 
+    }
+
+    @PostConstruct
+    public void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
