890338560aaf28d3cb64ac5508ad5f48776935d4
woojin126 <woojin126@naver.com>
2021-12-17 12:35:38 +0900

    BoardResponseDto add CommentResponseDto

--- src/main/java/com/example/be_velog_team11/dto/response/BoardResponseDto.java
+++ src/main/java/com/example/be_velog_team11/dto/response/BoardResponseDto.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Builder
 @Data
 @NoArgsConstructor
@@ -18,5 +20,16 @@ public class BoardResponseDto {
     private String img;
     private String createdAt;
     private Integer like;
+    private List<CommentResponseDto> commentResponseDtoList;
 
+
+    public BoardResponseDto(Long id, String title, String content, String nickname, String img, String createdAt, Integer like) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.nickname = nickname;
+        this.img = img;
+        this.createdAt = createdAt;
+        this.like = like;
+    }
 }
