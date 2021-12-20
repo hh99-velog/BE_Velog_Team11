06c778b046caf32134afec8914693ffd989f0bd3
woojin126 <woojin126@naver.com>
2021-12-20 15:36:16 +0900

    test

--- src/main/java/com/example/be_velog_team11/controller/UserController.java
+++ src/main/java/com/example/be_velog_team11/controller/UserController.java
@@ -19,6 +19,7 @@ public class UserController {
     public Long registerUser(@RequestBody SignupRequestDto signupRequestDto) {
         log.info("user_login={}",signupRequestDto.toString());
         return userService.registerUser(signupRequestDto);
+        //test
     }
 
     @ApiOperation("ID(Email) 중복확인")
